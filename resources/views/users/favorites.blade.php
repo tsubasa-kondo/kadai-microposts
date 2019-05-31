6b4e8c846cfd227533a402228b04fcf14afec4e1
tsubasa kondo <ec2-user@ip-172-31-94-246.ec2.internal>
2019-05-31 08:32:25 +0000

    favorite_button

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -10,6 +10,7 @@
             
             @if (count($microposts) > 0)
                 @include('microposts.microposts', ['microposts' => $microposts])
+               
             @endif
         </div>
     </div>
